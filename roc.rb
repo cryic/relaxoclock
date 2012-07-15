39af60afcb8eb002f68ae299751f1cf3033448ec
mat <matclutter@gmail.com>
2012-07-15 04:15:44 +0000

    started using shotgun; removed ip and port from roc.rb

--- roc.rb
+++ roc.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
 
-set :bind, '50.116.52.79'
-set :port, '1234'
-
 get '/' do
     # "at the sound of the gong, the time will be #{Time.now}."
-    erb :index, :locals => {:vid_src => 'http://www.youtube.com/embed/QBNIvqXmdKk'}
+    #erb :index, :locals => {:vid_src => 'http://www.youtube.com/embed/QBNIvqXmdKk'}
+#    @vid_src = 'http://www.youtube.com/embed/QBNIvqXmdKk'
+    @vid_src = ''
+    erb :index
 end
 
