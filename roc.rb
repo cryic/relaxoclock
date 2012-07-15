e0fe0a4c451262529ddf66dbe092a47b320dd250
mat <matclutter@gmail.com>
2012-07-15 01:54:44 +0000

    passing link via erb params

--- roc.rb
+++ roc.rb
@@ -5,6 +5,6 @@ set :port, '1234'
 
 get '/' do
     # "at the sound of the gong, the time will be #{Time.now}."
-    erb :index
+    erb :index, :locals => {:vid_src => 'http://www.youtube.com/embed/QBNIvqXmdKk'}
 end
 
